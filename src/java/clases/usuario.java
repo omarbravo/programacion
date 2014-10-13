249060548183afde9bcef5127bd1779cb20137d3
omarbravo <omarbravo13@live.com>
2014-10-13 12:44:26 -0500

    paquete de clases

---
+++ src/java/clases/usuario.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author Omar
+ */
+public class usuario {
+    
+    private String nombre;
+
+    /**
+     * Get the value of nombre
+     *
+     * @return the value of nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @param nombre new value of nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    private String apellido;
+
+    /**
+     * Get the value of apellido
+     *
+     * @return the value of apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * Set the value of apellido
+     *
+     * @param apellido new value of apellido
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    private String telefono;
+
+    /**
+     * Get the value of telefono
+     *
+     * @return the value of telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * Set the value of telefono
+     *
+     * @param telefono new value of telefono
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    private String stringdireccion;
+
+    /**
+     * Get the value of stringdireccion
+     *
+     * @return the value of stringdireccion
+     */
+    public String getStringdireccion() {
+        return stringdireccion;
+    }
+
+    /**
+     * Set the value of stringdireccion
+     *
+     * @param stringdireccion new value of stringdireccion
+     */
+    public void setStringdireccion(String stringdireccion) {
+        this.stringdireccion = stringdireccion;
+    }
+    
+}
